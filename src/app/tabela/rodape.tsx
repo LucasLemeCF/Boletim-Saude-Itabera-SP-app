5008fb6adf540ba0a93eb9515105629a05c59757
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2024-08-08 10:45:46 -0300

    Corrigido porcentagem do rodape

--- src/app/tabela/rodape.tsx
+++ src/app/tabela/rodape.tsx
@@ -1,40 +1,40 @@
 
 export function Rodape({dadosTabela, linhasTabela}) {
-    let totalDia = somarAtendimentosDia(linhasTabela);
-    const totalMetaDia = somarMetaDia(dadosTabela);
-    let totalAtingidoDia = calculaPorcentagemDia(dadosTabela);
+  let totalDia = somarAtendimentosDia(linhasTabela);
+  const totalMetaDia = somarMetaDia(dadosTabela);
+  let totalAtingidoDia = calculaPorcentagemDia(dadosTabela);
 
-    let totalMes = somarAtendimentosMes(dadosTabela) + totalDia;
-    const totalMetaMes = somarMetaMes(dadosTabela);
-    let totalAtingidoMes = calcularPorcentagemMes(dadosTabela);
+  let totalMes = somarAtendimentosMes(dadosTabela) + totalDia;
+  const totalMetaMes = somarMetaMes(dadosTabela);
+  let totalAtingidoMes = calcularPorcentagemMes(dadosTabela);
 
-    return (
-      <div className="flex border border-t-0 divide-x border-black bg-[#337B5B] w-full">
-        <div className="flex items-center justify-center border-black w-[300px]">
-          <p className="font-semibold text-white">Total</p>
-        </div>
-  
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalDia}</p>
-        </div>
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalMetaDia}</p>
-        </div>
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalAtingidoDia}%</p>
-        </div>
-  
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalMes}</p>
-        </div>
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalMetaMes}</p>
-        </div>
-        <div className="flex items-center justify-center border-black w-[100px]">
-          <p className="font-semibold text-white">{totalAtingidoMes}%</p>
-        </div>
+  return (
+    <div className="flex border border-t-0 divide-x border-black bg-[#337B5B] w-full">
+      <div className="flex items-center justify-center border-black w-[300px]">
+        <p className="font-semibold text-white">Total</p>
       </div>
-    )
+
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalDia}</p>
+      </div>
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalMetaDia}</p>
+      </div>
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalAtingidoDia}%</p>
+      </div>
+
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalMes}</p>
+      </div>
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalMetaMes}</p>
+      </div>
+      <div className="flex items-center justify-center border-black w-[100px]">
+        <p className="font-semibold text-white">{totalAtingidoMes}%</p>
+      </div>
+    </div>
+  )
 }
 
 function somarAtendimentosDia(linhasTabela) {
@@ -52,39 +52,39 @@ function somarAtendimentosDia(linhasTabela) {
 }
   
 function somarMetaDia(dadosTabela) {
-    let totalMetaDia = 0;
-    
-    dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
-        cabecalho.especialidades.map((especialidade) => {
-          totalMetaDia += especialidade.metaDiaria;
-        });
-    });
-    
-    return totalMetaDia;
+  let totalMetaDia = 0;
+  
+  dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
+      cabecalho.especialidades.map((especialidade) => {
+        totalMetaDia += especialidade.metaDiaria;
+      });
+  });
+  
+  return totalMetaDia;
 }
 
 function somarAtendimentosMes(dadosTabela) {
-    let totalMes = 0;
+  let totalMes = 0;
 
-    dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
-        cabecalho.especialidades.map((especialidade) => {
-          totalMes += especialidade.pacientesAtendidosMes;
-        });
-    });
+  dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
+      cabecalho.especialidades.map((especialidade) => {
+        totalMes += especialidade.pacientesAtendidosMes;
+      });
+  });
 
-    return totalMes;
+  return totalMes;
 }
 
 function somarMetaMes(dadosTabela) {
-    let totalMetaMes = 0;
+  let totalMetaMes = 0;
 
-    dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
-        cabecalho.especialidades.map((especialidade) => {
-          totalMetaMes += especialidade.metaMensal;
-        });
-    });
+  dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
+      cabecalho.especialidades.map((especialidade) => {
+        totalMetaMes += especialidade.metaMensal;
+      });
+  });
 
-    return totalMetaMes;
+  return totalMetaMes;
 }
 
 function calculaPorcentagemDia(dadosTabela) {
@@ -92,29 +92,29 @@ function calculaPorcentagemDia(dadosTabela) {
     let qtdEspecialidades = 0;
 
     dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
-        cabecalho.especialidades.map((especialidade) => {
-          if (especialidade.metaDiaria > 0) {
-            totalPorcentagemDia += Number(especialidade.pacientesAtendidos / Number(especialidade.metaDiaria));
-            qtdEspecialidades++;
-          }
-        });
+      cabecalho.especialidades.map((especialidade) => {
+        if (especialidade.metaDiaria > 0) {
+          totalPorcentagemDia += Number(especialidade.pacientesAtendidosDia / Number(especialidade.metaDiaria));
+          qtdEspecialidades++;
+        }
+      });
     });
 
     return ((totalPorcentagemDia/qtdEspecialidades) * 100).toFixed(2);
 }
 
 function calcularPorcentagemMes(dadosTabela) {
-    let totalPorcentagemMes = 0;
-    let qtdEspecialidades = 0;
+  let totalPorcentagemMes = 0;
+  let qtdEspecialidades = 0;
 
-    dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
-        cabecalho.especialidades.map((especialidade) => {
-          if (especialidade.metaMensal > 0) {
-            totalPorcentagemMes += Number(especialidade.pacientesAtendidosMes / Number(especialidade.metaMensal));
-            qtdEspecialidades++;
-          }
-        });
+  dadosTabela.especialidadesCabecalhos.map((cabecalho) => {
+    cabecalho.especialidades.map((especialidade) => {
+      if (especialidade.metaMensal > 0) {
+        totalPorcentagemMes += Number(especialidade.pacientesAtendidosMes / Number(especialidade.metaMensal));
+        qtdEspecialidades++;
+      }
     });
+  });
 
-    return ((totalPorcentagemMes/qtdEspecialidades) * 100).toFixed(2);
+  return ((totalPorcentagemMes/qtdEspecialidades) * 100).toFixed(2);
 } 
